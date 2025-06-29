1dec3ec3db1ab1efc8eafbb6e4a4c5a7b3c03a81
Utabus <nguyenchikhang85@gmail.com>
2025-06-29 12:25:12 +0700

    up code

---
+++ TranMinhKhoi_com_vn/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TranMinhKhoi_com_vn.Models;
+
+namespace TranMinhKhoi_com_vn.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TranMinhKhoi_com_vn/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TranMinhKhoi_com_vn.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
